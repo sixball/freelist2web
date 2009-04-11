52aebcda25f7c1a6536296e21ea3884f305aeeeb
sixball <sixball@5cf79762-26dd-11de-8b7d-1d3e1581ed71>
2009-04-11 21:33:30 +0000

    Trivial test import.

---
+++ faq.php
@@ -0,0 +1,5 @@
+<h2>Connecting 'Offered' and 'Taken' posts</h2><a name="linking">
+The system tries to work out if an offered item has been taken.  It does this by comparing looking for later 'taken' posts from the same e-mail and comparing the subject headers.  If they loosely match then it connects them and marks the item as taken.  It's not a perfect solution because possible differences in spelling or sender address.  This is why the 'taken' list is also included: it includes many posts for which the original offer cannot be found.
+
+<h2>What about privacy?</h2>
+Although the database records the email of each poster, we only show the part before the @ symbol.  There are different domains after the @ (and this also how Yahoo! handles privacy on the web).
